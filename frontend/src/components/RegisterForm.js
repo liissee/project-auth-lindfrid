688f78b96e9d959265de21fda976c5605707333d
Frida Jonsson <annafridamariajonsson@gmail.com>
2020-02-20 16:31:06 +0100

    Some front end validation

--- frontend/src/components/RegisterForm.js
+++ frontend/src/components/RegisterForm.js
@@ -1,7 +1,7 @@
 // create form to create new user
 //Frida
 
-import React, {useState} from 'react'
+import React, {useState, Link} from 'react'
 import styled from 'styled-components'
 import { Heading, FieldContainer, Form, Button, Input } from "./Styling"
 
@@ -11,6 +11,7 @@ export const RegisterForm = () => {
 const [name, setName] = useState("")
 const [password, setPassword] = useState("")
 const [email, setEmail] = useState("")
+const [registred, setRegistred] = useState(false)
 
 const handleSubmit = event => {
   event.preventDefault()
@@ -19,68 +20,67 @@ const handleSubmit = event => {
     body: JSON.stringify({ name, email, password }),
     headers: { 'Content-Type': 'application/json'}
   })
+    // .then(res => {
+    //   if (res.status !== 200) {
+    //     throw new Error("Not 200");
+    //   } else {
+    //     return res;
+    //   } })
     .then(res => res.json())
-    .then(json => console.log(json))
+    .then(setRegistred(true))
     .catch(err => console.log('Error:', err))
 }
     return (
+      
       <div>
-      <h2>Register form</h2>
-      <form onSubmit={handleSubmit}>
-          <p>Name</p>
-          <label>
-            <input
-              type="text"
-              required
-              value={name}
-              onChange={event => setName(event.target.value)}>
-            </input>
-          </label>
-          <p>Email</p>
-          <label>
-            <input
-              type="text"
-              required
-              value={email}
-              onChange={event => setEmail(event.target.value)}>
-            </input>
-          </label>
-          <p>Password</p>
-          <label>
-            <input
-              type="password"
-              required
-              value={password}
-              onChange={event => setPassword(event.target.value)}>
-            </input>
-          </label>
-          
-          <button type="submit" 
-          onClick={handleSubmit} 
-          // disabled={name && password ? true : false}
-          >
-              SIGN UP
-          </button>
-      </form>
+      {!registred && ( 
+      <div>
+        <h2>Register form</h2>
+        <form onSubmit={handleSubmit}>
+            <p>Name</p>
+            <label>
+              <input
+                type="text"
+                required
+                value={name}
+                onChange={event => setName(event.target.value)}>
+              </input>
+            </label>
+            <p>Email</p>
+            <label>
+              <input
+                type="text"
+                required
+                value={email}
+                onChange={event => setEmail(event.target.value)}>
+              </input>
+            </label>
+            <p>Password</p>
+            <label>
+              <input
+                type="password"
+                required
+                value={password}
+                onChange={event => setPassword(event.target.value)}>
+              </input>
+            </label>
+            
+            <button type="submit" 
+            disabled={name.length > 1 && name.length < 21 && password.length > 4 && email ? false : true}
+            onClick={handleSubmit}>
+                SIGN UP
+            </button>
+        </form>
+      </div>
+    )}
+    {registred && (
+      <div>
+        <h2>Registration done!</h2>
+        {/* <Link to={`/login`}>
+          <h3>Log in!</h3>
+        </Link> */}
+      </div>
+    )}
     </div>
-      // <FieldContainer>
-      //   <Heading>Register form</Heading>
-      //   <Form
-      //   onKeyPress={(e) => {
-      //       if (e.key === 'Enter') { handleSubmit(name) }
-      //     }}>
-      //       <Input
-      //         type="text"
-      //         onChange={event => setName(event.target.value)}>
-      //       </Input>
-      //       {/* <Input
-      //         type="password"
-      //         onChange={event => setName(event.target.value)}>
-      //       </Input> */}
-      //       <Button type="submit" onClick={handleSubmit} disabled={name.length < 5 || name.length > 40 ? true : false}>
-      //           Submit
-      //       </Button>
-      //   </Form>
-      // </FieldContainer>
     )
 }
