4a778a54d9ff32ee16706dab630f440686d136b4
Linda <isell.linda@gmail.com>
2020-02-22 17:10:31 +0100

    Fix error message when wrong usename or password

--- frontend/src/components/StartPage.js
+++ frontend/src/components/StartPage.js
@@ -1,10 +1,57 @@
-import React from 'react'
-import { } from "./Styling"
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom"
+import { Button, Heading, FieldContainer, Wrapper } from "./Styling"
 
 
-export const StartPage = () => {
+const url = "http://localhost:8080/secrets";
+
+export const StartPage = props => {
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  //Getting the accessToken from the browser's localStorage
+  //and sending it as the header "Authorization"
+  const accessToken = window.localStorage.getItem("accessToken");
+
+  useEffect(() => {
+    setErrorMessage("");
+    fetch(url, {
+      method: "GET",
+      headers: { Authorization: accessToken }
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw new Error("Access denied", JSON);
+        } else {
+          return res.json();
+        }
+      })
+      .then(json => setMessage(json.secret))
+      .catch(err => {
+        setErrorMessage(err.message);
+      });
+  }, [accessToken]);
 
   return (
-    <div>I'm a frontend startpage! Welcome.</div>
+    <Wrapper>
+      <div>
+        <Heading>You're logged in!</Heading>
+        <Heading>{message}</Heading>
+      </div>
+      <div>
+        <img src="https://i.giphy.com/media/d7rSiIwVhNMIURGiMw/giphy.webp"></img>
+      </div>
+      <div>
+        {errorMessage && <div>{errorMessage}</div>}
+        <Button
+          onClick={() => window.localStorage.removeItem("accessToken")}
+          type="button"
+        >
+          <Link to={`/login`}>
+            Log out</Link>
+        </Button>
+      </div>
+
+    </Wrapper>
   )
 }
